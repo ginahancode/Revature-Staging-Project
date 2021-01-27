663afe3a882bd1338a69682529f1612569248fef
jvassas13 <jakevassas@gmail.com>
2021-01-27 11:09:02 -0500

    create springboot/java backend

---
+++ staging-project-service/src/main/java/com/example/stagingprojectservice/StagingProjectServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.stagingprojectservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StagingProjectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StagingProjectServiceApplication.class, args);
+	}
+
+}
---
+++ staging-project-service/src/test/java/com/example/stagingprojectservice/StagingProjectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stagingprojectservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StagingProjectServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
