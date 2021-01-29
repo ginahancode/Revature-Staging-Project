0d17fcaf6c084b2290192c58df0d362910f32122
jvassas13 <jakevassas@gmail.com>
2021-01-29 14:17:51 -0500

    User Controller getAllUsers method

--- staging-project-service/src/main/java/com/example/stagingprojectservice/controllers/UserController.java
+++ staging-project-service/src/main/java/com/example/stagingprojectservice/controllers/UserController.java
@@ -1,4 +1,24 @@
 package com.example.stagingprojectservice.controllers;
 
+import com.example.stagingprojectservice.entities.User;
+import com.example.stagingprojectservice.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("/api/v1")
 public class UserController {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping("/users")
+    @ResponseBody
+    public List<User> getAllUsers() {
+        return userRepo.findAll();
+    }
+
 }
