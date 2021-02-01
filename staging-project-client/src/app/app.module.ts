746b2c790665d7a6b2e3201a09b257f43fb02f42
jvassas13 <jakevassas@gmail.com>
2021-02-01 12:26:15 -0500

    Create services, models and components directory w/ components, also add Bootstrap

--- staging-project-client/src/app/app.module.ts
+++ staging-project-client/src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { ProductComponent } from './components/product/product.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { LoginComponent } from './components/login/login.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FilterComponent } from './components/filter/filter.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LandingComponent,
+    ShopComponent,
+    ProductComponent,
+    CartComponent,
+    CheckoutComponent,
+    LoginComponent,
+    AdminComponent,
+    FilterComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
